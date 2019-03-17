983bb31c4913c6a692312e1310dfc9e7616f7777
Dimitri Pankov <dimon2255@gmail.com>
2019-03-17 13:25:40 -0400

    DST Demo

--- DSTDemo/Program.cs
+++ DSTDemo/Program.cs
@@ -6,7 +6,20 @@ namespace DSTDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime utcNow = DateTime.UtcNow;
+            TimeZoneInfo sydneyTimezone = TimeZoneInfo.FindSystemTimeZoneById("AUS Eastern Standard Time");
+            DateTime sydneyTime = TimeZoneInfo.ConvertTimeFromUtc(utcNow, sydneyTimezone);
+
+           // Console.WriteLine(sydneyTime);
+
+
+            DateTime newDate = new DateTime(2010, 1, 1,  5, 59, 59);
+            var date = DateTime.SpecifyKind(newDate, DateTimeKind.Utc);
+
+
+            Console.WriteLine(TimeZoneInfo.ConvertTime(date, sydneyTimezone));
+
+            Console.ReadLine();
         }
     }
 }
